1127692d10f6169b1fa7d5d78bf8477a08923e0b
Adrien Cahen <onlineallthetime@gmail.com>
2015-03-15 18:15:38 -0700

    mk www if not exist

--- phonegap/setup.sh
+++ phonegap/setup.sh
@@ -4,6 +4,7 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 CORDOVA_BIN="${DIR}/../node_modules/.bin/cordova"
 
 cd ${DIR}
+[ -d www ] || mkdir www
 ${CORDOVA_BIN} platform add browser
 ${CORDOVA_BIN} platform add android
 ${CORDOVA_BIN} platform add ios
